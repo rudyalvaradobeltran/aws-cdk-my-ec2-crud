8e535e635804aa240ad25aa9eb82b463cc746a2c
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2025-04-19 20:23:19 -0400

    S3 bucket and permissions

--- deploy/lib/webapp-ec2-stack.ts
+++ deploy/lib/webapp-ec2-stack.ts
@@ -59,7 +59,19 @@ export class WebappEc2Stack extends Stack {
           'arn:aws:s3:::temp-webapp-deployment-*/*',
         ],
       })
-    );    
+    );
+    
+    ec2Role.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "s3:PutObject"
+        ],
+        resources: [
+          "arn:aws:s3:::webapp-install-logs/*"
+        ]
+      })
+    )
 
     const machineImage = new AmazonLinuxImage({
       generation: AmazonLinuxGeneration.AMAZON_LINUX_2023,
