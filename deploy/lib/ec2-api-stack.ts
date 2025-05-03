0a7ca37571c790108ceb58a4da7ac90887108ecc
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2025-05-03 19:54:59 -0400

    Application load balancer for the API

--- deploy/lib/ec2-api-stack.ts
+++ deploy/lib/ec2-api-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps, RemovalPolicy } from "aws-cdk-lib";
+import { Stack, StackProps, RemovalPolicy, Duration, CfnOutput } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import {
   Vpc,
@@ -10,11 +10,21 @@ import {
   InstanceClass,
   InstanceSize,
   AmazonLinuxImage,
-  AmazonLinuxGeneration
+  AmazonLinuxGeneration,
+  SubnetType
 } from "aws-cdk-lib/aws-ec2";
 import { Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
+import {
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  ApplicationTargetGroup,
+  TargetType,
+  ListenerAction,
+  IpAddressType
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { InstanceTarget } from "aws-cdk-lib/aws-elasticloadbalancingv2-targets/lib";
 
 interface Ec2ApiStackProps extends StackProps {
   VPC: Vpc;
@@ -34,11 +44,18 @@ export class Ec2ApiStack extends Stack {
 
     const privateInstanceSG = new SecurityGroup(this, "PrivateInstanceSG", {
       vpc: VPC,
-      description: "Allow private access"
+      description: "Allow access from ALB only"
     });
 
-    privateInstanceSG.addIngressRule(Peer.anyIpv4(), Port.tcp(80), "Allow HTTP");
-    privateInstanceSG.addIngressRule(Peer.anyIpv4(), Port.tcp(443), "Allow HTTPS");
+    const albSG = new SecurityGroup(this, "AlbSG", {
+      vpc: VPC,
+      description: "Allow public access to ALB"
+    });
+
+    albSG.addIngressRule(Peer.anyIpv4(), Port.tcp(80), "Allow HTTP from anywhere");
+    albSG.addIngressRule(Peer.anyIpv4(), Port.tcp(443), "Allow HTTPS from anywhere");
+    
+    privateInstanceSG.addIngressRule(albSG, Port.tcp(80), "Allow HTTP from ALB only");
     privateInstanceSG.addEgressRule(Peer.anyIpv4(), Port.allTraffic(), "Allow All Egress");
 
     const privateSubnet = VPC.selectSubnets({
@@ -83,7 +100,7 @@ export class Ec2ApiStack extends Stack {
     const machineImage = new AmazonLinuxImage({
       generation: AmazonLinuxGeneration.AMAZON_LINUX_2023,
     });
-
+    
     const APIInstance = new Instance(this, "APIInstance", {
       vpc: VPC,
       instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
@@ -93,9 +110,46 @@ export class Ec2ApiStack extends Stack {
       role: Ec2ApiRole
     });
 
+    const alb = new ApplicationLoadBalancer(this, 'ApiALB', {
+      vpc: VPC,
+      internetFacing: true,
+      securityGroup: albSG,
+      vpcSubnets: { subnetType: SubnetType.PUBLIC },
+      ipAddressType: IpAddressType.IPV4
+    });
+
+    const targetGroup = new ApplicationTargetGroup(this, 'ApiTargetGroup', {
+      vpc: VPC,
+      port: 80,
+      protocol: ApplicationProtocol.HTTP,
+      targetType: TargetType.INSTANCE,
+    });
+
+    const target = new InstanceTarget(APIInstance);
+    targetGroup.addTarget(target);
+
+    alb.addListener('HttpListener', {
+      port: 80,
+      defaultAction: ListenerAction.forward([targetGroup])
+    });
+
+    /* 
+    const certificate = Certificate.fromCertificateArn(this, 'Certificate', 'your-certificate-arn');
+    const httpsListener = alb.addListener('HttpsListener', {
+      port: 443,
+      certificates: [certificate],
+      defaultAction: ListenerAction.forward([targetGroup])
+    });
+    */
+
     new StringParameter(this, 'APIInstanceId', {
       parameterName: '/api/ec2-api-instance-id',
       stringValue: APIInstance.instanceId,
+    });
+
+    new CfnOutput(this, 'ApiEndpoint', {
+      description: 'The DNS name of the ALB',
+      value: alb.loadBalancerDnsName
     });
   }
 }
